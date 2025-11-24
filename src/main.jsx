d6ad78bb2947dd76c7a8aa100ceb812b75d5d95b
Eriwe-li <11926638+zakingslayerv22@users.noreply.github.com>
2025-11-24 10:49:18 -0500

    Import and render PokemonGame in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import PokemonList from "./components/PokemonList";
+import { PokemonGame } from "./components/PokemonGame";
+
 // import './index.css'
 // import App from './App.jsx'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
-    <PokemonList />
+    <PokemonGame />
   </StrictMode>
 );
